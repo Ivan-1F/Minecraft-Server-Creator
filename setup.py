51d1b90a4a0617f1624206655fbf8c5b81cf4794
Ivan1F <wyf0762@foxmail.com>
2021-04-29 17:45:14 +0800

    1.0.1 add docs

--- setup.py
+++ setup.py
@@ -4,18 +4,26 @@ import re
 
 from mcservercreator import constants
 
+# python setup.py sdist bdist_wheel
+# python -m twine upload --repository testpypi dist/*
+# python -m twine upload dist/*
+
 here = os.path.abspath(os.path.dirname(__file__))
 
 with open(os.path.join(here, 'requirements.txt')) as f:
     REQUIRED = [re.match(r'^[A-Za-z.]+', line).group() for line in f.readlines() if not len(line.strip()) == 0]
 
-print('REQUIRED = {}'.format(REQUIRED))
+# print('REQUIRED = {}'.format(REQUIRED))
 
+with open(os.path.join(here, 'README.md'), encoding='utf8') as f:
+    LONG_DESCRIPTION = f.read()
 
 setup(
     name=constants.PACKAGE_NAME,
     version=constants.VERSION,
     description=constants.DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
     author=constants.AUTHOR,
     python_requires='>=3.6.0',
     url=constants.GITHUB_REPO,
